9ef19b38da5fd16ebbe6e74670c4ac6287b4add2
robinz <robin.zhangx@gmail.com>
2014-05-20 16:40:07 +0800

    init commit.

---
+++ LruCache/LruCache.h
@@ -0,0 +1,41 @@
+//
+//  LruCache.h
+//
+//  Created by robin on 5/20/14.
+//  Copyright (c) 2014 Robin Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol LruCacheDelegate
+
+- (NSInteger)sizeOf:(NSString *)key value:(id)value;
+
+@optional
+- (id)create:(NSString *)key;
+- (void)entryRemoved:(BOOL)evicted key:(NSString *)key oldValue:(id)oldValue newValue:(id)newValue;
+
+@end
+
+@interface LruCache : NSObject
+
+- (id)initWithMaxSize:(NSInteger)maxSize;
+- (void)resize:(NSInteger)maxSize;
+- (id)get:(NSString *)key;
+- (id)put:(NSString *)key value:(id)value;
+- (void)trimToSize:(NSInteger)maxSize;
+- (id)remove:(NSString *)key;
+- (void)evictAll;
+- (NSMutableDictionary *)snapshot;
+
+@property (readonly) NSInteger size;
+@property (readonly) NSInteger maxSize;
+@property (readonly) NSInteger hitCount;
+@property (readonly) NSInteger missCount;
+@property (readonly) NSInteger createCount;
+@property (readonly) NSInteger putCount;
+@property (readonly) NSInteger evictionCount;
+
+@property (nonatomic, weak) id<LruCacheDelegate> delegate;
+
+@end
